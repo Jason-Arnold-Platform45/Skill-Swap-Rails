826391af78277c60c6095574111606cf1416eb11
Jason-Arnold-Platform45 <jason.arnold@platform45.com>
2026-01-20 09:37:37 +0200

    added more views

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  unauthenticated do
-    root to: "devise/sessions#new", as: :unauthenticated_root
+  devise_scope :user do
+    unauthenticated do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
   end
 
   authenticated :user do
     root to: "skills#index", as: :authenticated_root
   end
 
-  resources :users, only: [:index, :show]
   resources :skills
-
   resources :matches do
     member do
       patch :accept
       patch :reject
     end
   end
+
+  resources :users, only: [:index, :show]
 end
